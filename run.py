d85e317edf13e8e0524663536fae9660129930b1
hgoyal254 <47057631+hgoyal254@users.noreply.github.com>
2021-02-13 15:27:45 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ import spotify
 
 song_dic = songdict.createDic()
 def run():
-    token = "BQDWdI1Z6SYAHUfwuLvLZg_XQeC4IZPfNvaX8wziYY4z-_6T3e31T5YNhU8u292kE_PDyBu9nb0MnTBv6GwSC7LyC5-DmJS3ZrpqqpK2iMfyvmidQ6z72BXSjLdrbRDYfRAIvzMdY3tVTyOJBVsrQaMDgps7bYrX1pafYhBuLmFqyw537vWWDisyQqIXvM8HZCCDNWafs4ZAaWF7p49ibIBHCaRBqrhAF3WfBvBnDccC-16ygozR-IX1GNFlwt86MNy-Ue9KGvvW3k8w2qcxPLsSSvO6V9vjhLM"
-    user_id = "g66a4g0hgc0nd0yk6djcv40l6"
+    #enter your userid and token
+    token = 
+    user_id = 
     spotify.add_song_playlist(token,song_dic,user_id)
         
 
